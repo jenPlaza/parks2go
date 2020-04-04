9aff87216edcea13a20c3432c3564e5645ea7080
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-04-04 19:35:45 -0400

    deleted extra code

--- cook2go/src/App.js
+++ cook2go/src/App.js
@@ -3,17 +3,10 @@ import Routes from './components/Routes';
 //React Router
 import{ BrowserRouter as Router }from 'react-router-dom'
 
-//Material UI
-import Paper from "@material-ui/core/Paper";
-
 function App() {
   return (
 	  <Router>
-    <div className="App">
-	  
-	  <Paper><Routes /></Paper>
-	  
-    </div>
+	  <Routes />
 	  </Router>
   );
 }
