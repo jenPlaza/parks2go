2043d5b51fd5999f18b93e640bcb96bd9310db6d
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-04-11 17:35:40 -0400

    pulled 5 parks from API and retrieved an image, address, entrance fees, park description, park activites, and directions for each park.

--- parks2go/src/pages/Main.js
+++ parks2go/src/pages/Main.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 
+
+
 //Smart Component
 class Main extends React.Component {
 	//declaring state and new object
 	state ={
-		recipeListing: []
+		parkListing: []
 	}
 
 //calling fetchData function
@@ -17,7 +19,7 @@ class Main extends React.Component {
 //fetch Api data and map json results in a list format
 	fetchData(){
 
-	fetch('https://developer.nps.gov/api/v1/parks?parkCode=afam&api_key=YpbDDtsNwQRi13JXZXiN7DnEIusWnKQLsCZW11xq')
+	fetch('https://developer.nps.gov/api/v1/parks?limit=5&api_key=YpbDDtsNwQRi13JXZXiN7DnEIusWnKQLsCZW11xq')
 		.then(results =>{
 		return results.json();
 	}).then(data =>{
@@ -28,22 +30,28 @@ class Main extends React.Component {
 				<span key={use.results}>
 				<img src={use.images[0].url} alt={use.images[0].altText} width="400" height="200"/>
 				<h2>{use.name}</h2>
-				<h6>{use.addresses[0].city}, {use.addresses[0].stateCode}</h6>
-
+				<h3>{use.addresses[0].city}, {use.addresses[0].stateCode}</h3>
 				<p><b>Park Details</b><br />
 					    Cost:{use.entranceFees[0].cost},<br />
 					    Description: {use.entranceFees[0].description},<br />
 					    Title: {use.entranceFees[0].title}</p>
-				
 				<p><b>Park Description</b><br /> {use.description}</p>
-				<p><b>Park Activities</b><br /> {use.activities}</p>
+				<p><b>Park Activities</b>
+					{use.activities.map(item =>{
+						return(
+							<div>{item.name}</div>	
+								)
+							})
+						}
+					</p>
 				<p><b>Park Info</b><br /> {use.directionsInfo}</p>
-				<Link href='{use.directionsUrl}'>{use.directionsUrl}</Link><br /><br /><br />
-
+				<p><b>For More Directions</b><br />
+				<Link href='{use.directionsUrl}'>{use.directionsUrl}</Link><br /><br /><br /><br /><br /></p>
 				</span>
 				</li>
 			)
 		})
+
 		this.setState({main:mList});
 	})
 }
