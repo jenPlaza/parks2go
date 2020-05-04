78ff9d49029c181a9af538293ba391ef6b335919
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-05-04 01:16:30 -0400

    more changes

--- parks2go/src/components/map/map.js
+++ parks2go/src/components/map/map.js
@@ -15,7 +15,8 @@ export default function Map() {
 	
 	 const mapHandler = (event) => {
 	  var targetId = event.target.dataset.name;
-	  window.location.assign(`https://jenplaza.github.io/parks2go/stateActivities/${targetId}`);
+	  //window.location.assign(`https://jenplaza.github.io/parks2go/stateActivities/${targetId}`);
+		 window.location.href='parks2go/stateActivities/${targetId}'
   };
 	
     return (
