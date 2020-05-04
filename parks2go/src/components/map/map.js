9a9423c58f922b2cfc169ba9cb5f6ce524cc0a83
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-05-04 00:51:26 -0400

    modified link back to http://localhost:3000 but added /parks2go/ to the link

--- parks2go/src/components/map/map.js
+++ parks2go/src/components/map/map.js
@@ -15,7 +15,7 @@ export default function Map() {
 	
 	 const mapHandler = (event) => {
 	  var targetId = event.target.dataset.name;
-	  window.location.assign(`http://jenplaza.github.io/parks2go/stateActivities/${targetId}`);
+	  window.location.assign(`http://localhost:3000/parks2go/stateActivities/${targetId}`);
   };
 	
     return (
