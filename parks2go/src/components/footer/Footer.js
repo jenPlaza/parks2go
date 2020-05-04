f77db54dddb5de4e4daefe31615935b6f54f2e97
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2020-05-04 02:55:26 -0400

    changes

--- parks2go/src/components/footer/Footer.js
+++ parks2go/src/components/footer/Footer.js
@@ -125,7 +125,7 @@ export default function Footer() {
 
 	  <Grid item xs={12} md={3} className={classes.about}>
 		<h2 className={classes.h2}>About Us</h2>
-					<p><Link className={classes.a} href='/about'>Parks2Go</Link> is a free interactive guide to National State Parks around the country, created by Jen Plaza. Parks2Go originally began as a React student assignment...</p>
+					<p><Link className={classes.a} href='/parks2go/about'>Parks2Go</Link> is a free interactive guide to National State Parks around the country, created by Jen Plaza. Parks2Go originally began as a React student assignment...</p>
         </Grid>
 	  
         <Grid item xs={12} md={8}>
